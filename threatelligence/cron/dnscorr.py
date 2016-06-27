08f62d867ed46451d177781536f0cba76dceb9ec
gfunkoriginal <gmcgibbney@gmail.com>
2016-06-27 22:18:45 +0100

    Correlation of DNS logs to malware DNS threat intel feed
    This script opens a txt file of DNS log and attempts to match the DNS
    queries within the file to the threat inel feed on DNS hosting malware.

--- threatelligence/cron/dnscorr.py
+++ threatelligence/cron/dnscorr.py
@@ -12,30 +12,19 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 '''
-
+Input commentary over what the code is intended to do.
 '''
 import sqlite3
 
-#Prepare database
-conn = sqlite3.connect('dnscorr.sqlite')
-cur = conn.cursor()
-
-cur.execute('''
-DROP TABLE IF EXISTS dns''')
-
-cur.execute('''
-CREATE TABLE dns (Date TEXT, Time TEXT, dns_request TEXT)''')
-
+dnsResponseCodes = ['NOERROR','FORMERR','SERVFAIL','NXDOMAIN','NOTIMP','REFUSED','YXDOMAIN','XRRSET','NOTAUTH','NOTZONE']
 
 fhand = open('test.txt')
 dnsDict = {}
 dnsList = ()
 iD = 0
-# data = fhand.find("]")
-# print data
 
 for line in fhand:
-    if 'NOERROR]' in line:
+    if dnsResponseCodes[0] in line:
         date = line[0:6]
         print date
         time = line[7:15]
@@ -47,13 +36,10 @@ for line in fhand:
         dns = newData.translate(None, "()123456789")
         dnsList = (date, time, dns)
         print dnsList
-        dnsDict[iD] = {dnsList}
+        dnsDict[iD] = dnsList
         print dnsDict
         iD += 1
 
-        #cur.execute('''INSERT INTO dns (Date,Time,dns_request)
-        #   VALUES (?, ?, ?)''', (date, time, dns))
-#conn.commit()
 print dnsDict
 
 try:
@@ -63,13 +49,5 @@ except:
     print "error"
 
 for dnsmatch in dnsDict:
-    result = cur.execute("SELECT * FROM database_servers WHERE InstalledApplications='%s' UNION ALL "
-                     "SELECT * FROM email_servers WHERE InstalledApplications='%s' UNION ALL "
-                     "SELECT * FROM dev_servers WHERE InstalledApplications='%s' UNION ALL "
-                     "SELECT * FROM domain_controllers WHERE InstalledApplications='%s' UNION ALL "
-                     "SELECT * FROM exchange WHERE InstalledApplications='%s' UNION ALL "
-                     "SELECT * FROM file_transfer WHERE InstalledApplications='%s' UNION ALL "
-                     "SELECT * FROM huxley WHERE InstalledApplications='%s' UNION ALL "
-                     "SELECT * FROM pas WHERE InstalledApplications = '%s'"
-                     % (dnsmatch, dnsmatch, dnsmatch, dnsmatch, dnsmatch, dnsmatch, dnsmatch, dnsmatch))
+    result = cur.execute("SELECT * FROM Collect WHERE domain='%s'" % dnsmatch)
 print result.fetchall()
